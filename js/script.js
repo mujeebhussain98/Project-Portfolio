8bfaca784902cfb57697e22a03a362f6123addff
mujeebhussain98 <mujeebhussain153@msitprogram.net>
2021-06-24 18:57:10 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Graduate", "Techie", "Programmer", "Web Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+});
